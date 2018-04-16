0ba937537bb1a711fa93f57e35975c7d695d63c6
vinaypatel25 <36524192+vinaypatel25@users.noreply.github.com>
2018-04-16 12:51:35 -0500

    Added Administrator, Board, Spaces, SPlayer, Tile

---
+++ Tsuro/SPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction { NONE, LEFT, RIGHT, UP, DOWN }
+
+public class SPlayer : MonoBehaviour {
+
+    public List<Tile> Hand { get; private set; }
+    public Vector2 Position { get; private set; }
+    public Vector2 PositionOnTile { get; private set; }
+
+    public void AddToHand(Tile t)
+    {
+        Hand.Add(t);
+        return;
+    }
+        
+    private string Color;
+
+    public bool IsInHand (Tile t)
+    {
+        if (Hand.Contains(t))
+        { return true; }
+        else { return false; }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Direction GetAdjacentDirection(Tile t)
+    {
+        return Direction.NONE;
+    }
+}
---
+++ Tsuro/Tile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Rotation { UP, LEFT, DOWN, RIGHT };
+
+public class Tile : MonoBehaviour {
+
+    public List<Vector2> Paths;
+    private Vector2 Coordinate;
+    private Rotation myrotation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	} 
+    public Tile SetRotation (Tile t, Rotation r)
+    {
+        t.myrotation = r;
+        return t;
+    }
+}
